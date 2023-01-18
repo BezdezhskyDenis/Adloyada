8648ecd9db35615a7e60a8475c09a6c933ddc8b4
Denis Bezdezhsky <denisb305@gmail.com>
2023-01-18 16:32:17 +0200

    WIP pageStructure

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 $("li").on("mouseover", function () {
-  $(this).addClass("border");
+  $(this).addClass("border rounded-pill");
   $(this).attr("aria-expanded", "true");
   $(this).children().addClass("show");
 });
 
 $("li").on("mouseout", function () {
-  $(this).removeClass("border");
+  $(this).removeClass("border rounded-pill");
   $(this).attr("aria-expanded", "false");
   $(this).children().removeClass("show");
+});
+
+$("a").on("click", function () {
+  var userChose = $(this).attr("id");
+  switch (userChose) {
+    case "adloyada":
+      $(this).attr("target", "_blank");
+      $(this).attr("href", "/");
+      break;
+    case "a":
+      tom2.play();
+      break;
+    case "s":
+      tom3.play();
+      break;
+    case "d":
+      tom4.play();
+      break;
+    case "j":
+      crash.play();
+      break;
+    case "k":
+      kickBass.play();
+      break;
+    case "l":
+      snare.play();
+      break;
+    default:
+      break;
+  }
 });
