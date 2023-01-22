b09dbbbee48d7db7f46e500ea4453da6d04dd215
Denis Bezdezhsky <denisb305@gmail.com>
2023-01-22 12:13:02 +0200

    WIP Gallery page

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ $("a").on("touchend touchstart tap click", function () {
       $(this).attr("href", "./about.html");
       break;
     case "gallery":
-      $(this).attr("href", "./");
+      $(this).attr("href", "./gallery.html");
       break;
     case "contactUs":
       $(this).attr("href", "./contactUs.html");
@@ -127,8 +127,6 @@ $(window).resize(function () {
 });
 
 
-
-
 // Set the date we're counting down to
 const countDownDate = new Date("Mar 6, 2023 09:30:00").getTime();
 
@@ -146,3 +144,59 @@ setInterval( ()=> {
   $('#clockMinutes').text(minutes);
   $('#clockSeconds').text(seconds);
 }, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
