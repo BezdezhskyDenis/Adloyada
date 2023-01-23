7667043a2590cf8e5533e1c699d652c066133714
Denis Bezdezhsky <denisb305@gmail.com>
2023-01-23 11:14:22 +0200

    finish gallery page

--- app.js
+++ app.js
@@ -145,50 +145,69 @@ setInterval( ()=> {
   $('#clockSeconds').text(seconds);
 }, 1000);
 
+// picture LightBox
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Open the Modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+function openModal(type) {
+  document.getElementById("myModal"+ type).style.display = "block";
 }
 
-// Close the Modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+function closeModal(type) {
+  document.getElementById("myModal" + type).style.display = "none";
 }
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
-  myModal.scrollTo(0, 0);
+  myModalPicture.scrollTo(0, 0);
 }
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  var slides = document.getElementsByClassName("mySlidesPic");
+  var dots = document.getElementsByClassName("demopic");
+  var captionText = document.getElementById("captionPicture");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+// Video LightBox
+
+
+var slideIndex = 1;
+showVideo(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showVideo(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentVideo(n) {
+  showVideo(slideIndex = n);
+  myModalVideo.scrollTo(0, 0);
+}
+
+function showVideo(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlidesVideo");
+  var dots = document.getElementsByClassName("demoVideo");
+  var captionText = document.getElementById("captionVideo");
+  // let playVideo = slides[slideIndex-1].children[1].getAttribute('src');
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
