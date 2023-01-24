8fbb7ef4f8326c1be1b5405391b0c99fcd35b57b
Denis Bezdezhsky <denisb305@gmail.com>
2023-01-24 15:14:23 +0200

    WIP about page and costume scss and js

--- app.js
+++ app.js
@@ -126,6 +126,32 @@ $(window).resize(function () {
   // $('#ww').text(viewportWidth)
 });
 
+$(document).ready(function () {
+  const viewportWidth = $(window).width();
+  // const viewportHeight = $(window).height();
+
+if (viewportWidth < 1000) {
+    $(".styleTriangle").attr('hidden','');
+    $(".floatLogo").removeAttr('hidden');
+  } else {
+    $(".floatLogo").attr('hidden', '');
+    $(".styleTriangle").removeAttr('hidden');
+  }
+});
+
+
+  $(window).resize(function () {
+    const viewportWidth = $(window).width();
+    // const viewportHeight = $(window).height();
+  
+    if (viewportWidth < 1000) {
+      $(".styleTriangle").attr('hidden','');
+      $(".floatLogo").removeAttr('hidden');
+    } else {
+      $(".floatLogo").attr('hidden','');
+      $(".styleTriangle").removeAttr('hidden');
+    }
+  });
 
 // Set the date we're counting down to
 const countDownDate = new Date("Mar 6, 2023 09:30:00").getTime();
@@ -145,78 +171,3 @@ setInterval( ()=> {
   $('#clockSeconds').text(seconds);
 }, 1000);
 
-// picture LightBox
-
-function openModal(type) {
-  document.getElementById("myModal"+ type).style.display = "block";
-}
-
-function closeModal(type) {
-  document.getElementById("myModal" + type).style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-  myModalPicture.scrollTo(0, 0);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlidesPic");
-  var dots = document.getElementsByClassName("demopic");
-  var captionText = document.getElementById("captionPicture");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-// Video LightBox
-
-
-var slideIndex = 1;
-showVideo(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showVideo(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentVideo(n) {
-  showVideo(slideIndex = n);
-  myModalVideo.scrollTo(0, 0);
-}
-
-function showVideo(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlidesVideo");
-  var dots = document.getElementsByClassName("demoVideo");
-  var captionText = document.getElementById("captionVideo");
-  // let playVideo = slides[slideIndex-1].children[1].getAttribute('src');
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
